9bac9504e2f6ac2f593f375c3e0a403262c6c0a6
xxshambm <ext.michael.shamberger@tieto.com>
2020-03-29 17:41:02 +0300

    try 100

--- run.py
+++ run.py
@@ -7,16 +7,13 @@ if len(sys.argv) <3:
     print("BASEDIR and country should be set")
     exit(1)
 basedir = sys.argv[1]
-country  = sys.argv[2]
+state  = sys.argv[2]
 path = basedir + "/s/synthea/build/resources/main/modules/"
 files = [f for f in listdir(path) if isfile(join(path, f))]
 
 # open file for writing markup file
 os.mkdir(basedir + '/s/synthea/output')
 os.chdir(basedir + '/s/synthea/output')
-file = open("README.md","w") 
-file.write("# " + country.upper() + " Download Page #\n")
-file.write("Healthcare synthetic dataset for the country of Finland in OMOP CDM 6.0 format.\n\n")
 
 regions = ['Uusimaa']
 
@@ -33,17 +30,17 @@ for region in regions:
             for f in filesToRemove:
                 os.remove(os.path.join('output/fhir', f))
     # run synthea
-    os.system("./run_synthea -p 10000")
+    os.system("./run_synthea -p 100")
     # compress synthea output
     os.chdir(basedir + '/s/synthea/output/csv')
     os.system("gzip *")
     # save the synthea data that will be used for omop conversion
     os.chdir(basedir + '/s/synthea/output/csv')
     os.system("ls -la")
-    os.system("zip " + basedir + "/s/ETL-Synthea-Python/" + country + "_covid19_synthea.zip *.csv*")
+    os.system("zip " + basedir + "/s/ETL-Synthea-Python/" + state + "_covid19_synthea.zip *.csv*")
     # run synthea->omop
     os.chdir(basedir + '/s/ETL-Synthea-Python/python_etl')
     os.system("python synthea_omop.py")
     os.chdir(basedir + '/s/ETL-Synthea-Python/output')
-    os.system("zip ../" + country + "_covid19_omop_6.zip *.csv")
+    os.system("zip ../" + state + "_covid19_omop_6.zip *.csv")
 file.close()
